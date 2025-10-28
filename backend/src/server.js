b8e4f0d52a06347daf9c342232845296e38281b4
Pedro H. Silva <pedrobhr7@gmail.com>
2025-10-28 18:33:20 -0300

    Remove pasta backend

--- backend/src/server.js
+++
@@ -1,5 +0,0 @@
-import sequelize from './config/database.js';
-
-sequelize.authenticate()
-  .then(() => console.log('Conexão com o MySQL estabelecida com sucesso!'))
-  .catch(err => console.error('Erro ao conectar no MySQL:', err));
